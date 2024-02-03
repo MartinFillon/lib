1298e8fe1b9cfaa5230885c9d3f59bd4b11c719a
MartinFillon <martin.fillon@hotmail.com>
2024-02-03 22:02:21 +0100

    tests: adding either tests

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -9,10 +9,17 @@
 #include "pipunit/include/Test.hpp"
 
 std::vector<pipunit::Test> JsonParserSpec();
+std::vector<pipunit::Test> EitherSpec();
 
 int main(void)
 {
     std::vector<pipunit::Test> suite = JsonParserSpec();
+    std::vector<pipunit::Test> eitherSuite = EitherSpec();
+
+    for (auto &test : eitherSuite) {
+        suite.push_back(test);
+    }
+
     if (!pipunit::printResult(suite)) {
         pipunit::printTests(suite);
         return 1;
